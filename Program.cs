63f00e19a78f1a48991937be55f03f08c989cf40
autotestpoc123 <autotestpoc123@163.com>
2026-09-17 08:22:13 +0800

    dafd

--- Program.cs
+++ Program.cs
@@ -85,3 +85,13 @@ public static class Program
         }
     }
 }
+
+ using System.Xml; using System.IO;
+  var xml = "<Root><Text2>7G754</Text2></Root>";
+  using var r = XmlReader.Create(new StringReader(xml), new XmlReaderSettings{IgnoreWhitespace=true});
+  r.MoveToContent();  r.Read();                 // 进入 <Text2>
+  while (!(r.NodeType==XmlNodeType.EndElement && r.LocalName=="Root")) {
+      if (r.NodeType==XmlNodeType.Element) { r.Read(); continue; }   // ← 原版逻辑
+      if (r.LocalName=="Text2") Console.WriteLine(r.ReadElementContentAsString());
+      else r.Skip();
+  }
